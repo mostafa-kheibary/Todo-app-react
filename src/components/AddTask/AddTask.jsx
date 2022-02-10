d1b0e54922a3dd9e9f80b96edb66d1f349403fe4
mostafa <mostafamnbvcxz5@gmail.com>
2022-02-10 21:35:15 +0330

    add form validation with toast error

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,10 +1,11 @@
 import TaskContext from '../context/TaskContext';
 import { useContext, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './AddTask.css';
 const AddTask = () => {
   const { addTasks } = useContext(TaskContext);
   const [text, setText] = useState({ topic: '', content: '' });
-
   const handleTopic = (e) => {
     setText({ ...text, topic: e.target.value });
   };
@@ -13,10 +14,18 @@ const AddTask = () => {
   };
   const handleSubmit = () => {
     setText({ topic: '', content: '' });
-    addTasks(text.topic, text.content);
+    if (text.topic.trim() !== '' && text.topic.trim() !== '') {
+      addTasks(text.topic, text.content);
+    } else {
+      errorMessage();
+    }
+  };
+  const errorMessage = () => {
+   toast.error('Please fill the fallowing form');
   };
   return (
     <div className='add-task'>
+      <ToastContainer />
       <div className='add-task__title'>
         <img
           className='add-task__title__icon'
@@ -26,20 +35,29 @@ const AddTask = () => {
         <h2 className='add-task__title__text'>Make New Task</h2>
       </div>
       <div className='add-task__inputs'>
-        <input
-          value={text.topic}
-          onChange={handleTopic}
-          className='input add-task__inputs__name'
-          type='text'
-          placeholder='your task topic'
-        />
-        <textarea
-        value={text.content}
-          onChange={handleContent}
-          className='input add-task__inputs__content'
-          type='text'
-          placeholder='more info about task'
-        />
+        <div>
+          <p className='add-task__lether-count'>{text.topic.length}/50</p>
+          <input
+            maxLength={50}
+            value={text.topic}
+            onChange={handleTopic}
+            className='input add-task__inputs__name'
+            type='text'
+            placeholder='your task topic'
+          />
+        </div>
+        <div>
+          <p className='add-task__lether-count'>{text.content.length}/150</p>
+          <textarea
+            maxLength={150}
+            value={text.content}
+            onChange={handleContent}
+            className='input add-task__inputs__content'
+            type='text'
+            placeholder='more info about task'
+          />
+        </div>
+
         <button onClick={handleSubmit} className='add-task__inputs__submit'>
           Create New Task
         </button>
