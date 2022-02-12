d0a10a06cb0bd75e04ffe44b7e6d7ba925d26803
mostafa <mostafamnbvcxz5@gmail.com>
2022-02-12 23:02:15 +0330

    add overlay for popup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import AddTask from './components/AddTask/AddTask';
 import Header from './components/Header/Header';
 import { TaskProvider } from './components/context/TaskContext';
 import PopUp from './components/PopUp/PopUp';
+import OverLay from './components/OverLay/OverLay';
 const App = () => {
   return (
     <TaskProvider>
       <PopUp />
+      <OverLay />
       <Header />
       <div className='container'>
         <AddTask />
