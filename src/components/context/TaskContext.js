5b22e8fc999fb989fb92390147b872dcbb4097bd
mostafa <mostafamnbvcxz5@gmail.com>
2022-02-13 12:29:46 +0330

    add local storage

--- src/components/context/TaskContext.js
+++ src/components/context/TaskContext.js
@@ -2,18 +2,27 @@ import { v4 as uuid4 } from 'uuid';
 import { createContext, useState, useEffect } from 'react';
 const TaskContext = createContext();
 export const TaskProvider = ({ children }) => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(
+    !localStorage.getItem('tasks')
+      ? localStorage.setItem('tasks', JSON.stringify([]))
+      : []
+  );
   const [doneTaskList, setDoneTask] = useState([]);
   const [PopUp, setPopUp] = useState({ in: false, item: null });
   const addTasks = (topic, content) => {
+    const oldTask = JSON.parse(localStorage.getItem('tasks'));
     const newTask = {
       id: uuid4(),
       isDone: false,
       topic,
       content,
     };
-    setTasks([newTask,...tasks]);
+    localStorage.setItem('tasks', JSON.stringify([newTask, ...oldTask]));
+    setTasks([newTask, ...tasks]);
   };
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem('tasks')));
+  }, []);
   useEffect(() => {
     const alldoneTask = tasks.filter((task) => task.isDone === true);
     setDoneTask(alldoneTask);
@@ -21,12 +30,14 @@ export const TaskProvider = ({ children }) => {
   const deleteTask = (id) => {
     const newTask = tasks.filter((task) => task.id !== id);
     setTasks(newTask);
+    localStorage.setItem('tasks', JSON.stringify(newTask));
   };
   const doneTask = (id) => {
     const newTask = [...tasks];
     const index = newTask.findIndex((task) => task.id === id);
     newTask[index].isDone = !newTask[index].isDone;
     setTasks(newTask);
+    localStorage.setItem('tasks', JSON.stringify(newTask));
   };
   const setId = (id) => {
     const index = tasks.findIndex((task) => task.id === id);
@@ -37,6 +48,7 @@ export const TaskProvider = ({ children }) => {
     const index = newTask.findIndex((task) => task.id === PopUp.item.id);
     newTask[index].topic = text;
     setTasks(newTask);
+    localStorage.setItem('tasks', JSON.stringify(newTask));
   };
   return (
     <TaskContext.Provider
