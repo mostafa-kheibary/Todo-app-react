f687c83d50645a0a098eed6f1d8e108934c70c7c
Mostafa Kheibary <58364608+mostafa-kheibary@users.noreply.github.com>
2022-02-27 16:16:02 +0330

    fix clear all bug

--- src/components/context/TaskContext.js
+++ src/components/context/TaskContext.js
@@ -1,27 +1,27 @@
-import { v4 as uuid4 } from 'uuid';
-import { createContext, useState, useEffect } from 'react';
+import { v4 as uuid4 } from "uuid";
+import { createContext, useState, useEffect } from "react";
 const TaskContext = createContext();
 export const TaskProvider = ({ children }) => {
   const [tasks, setTasks] = useState(
-    !localStorage.getItem('tasks')
-      ? localStorage.setItem('tasks', JSON.stringify([]))
+    !localStorage.getItem("tasks")
+      ? localStorage.setItem("tasks", JSON.stringify([]))
       : []
   );
   const [doneTaskList, setDoneTask] = useState([]);
   const [PopUp, setPopUp] = useState({ in: false, item: null });
   const addTasks = (topic, content) => {
-    const oldTask = JSON.parse(localStorage.getItem('tasks'));
+    const oldTask = JSON.parse(localStorage.getItem("tasks"));
     const newTask = {
       id: uuid4(),
       isDone: false,
       topic,
-      content,
+      content
     };
-    localStorage.setItem('tasks', JSON.stringify([newTask, ...oldTask]));
+    localStorage.setItem("tasks", JSON.stringify([newTask, ...oldTask]));
     setTasks([newTask, ...tasks]);
   };
   useEffect(() => {
-    setTasks(JSON.parse(localStorage.getItem('tasks')));
+    setTasks(JSON.parse(localStorage.getItem("tasks")));
   }, []);
   useEffect(() => {
     const alldoneTask = tasks.filter((task) => task.isDone === true);
@@ -30,17 +30,18 @@ export const TaskProvider = ({ children }) => {
   const deleteTask = (id) => {
     const newTask = tasks.filter((task) => task.id !== id);
     setTasks(newTask);
-    localStorage.setItem('tasks', JSON.stringify(newTask));
+    localStorage.setItem("tasks", JSON.stringify(newTask));
   };
-  const deleteAll = ()=>{
+  const deleteAll = () => {
     setTasks([]);
-  }
+    localStorage.setItem("tasks", JSON.stringify([]));
+  };
   const doneTask = (id) => {
     const newTask = [...tasks];
     const index = newTask.findIndex((task) => task.id === id);
     newTask[index].isDone = !newTask[index].isDone;
     setTasks(newTask);
-    localStorage.setItem('tasks', JSON.stringify(newTask));
+    localStorage.setItem("tasks", JSON.stringify(newTask));
   };
   const setId = (id) => {
     const index = tasks.findIndex((task) => task.id === id);
@@ -52,7 +53,7 @@ export const TaskProvider = ({ children }) => {
     newTask[index].topic = text.topic;
     newTask[index].content = text.content;
     setTasks(newTask);
-    localStorage.setItem('tasks', JSON.stringify(newTask));
+    localStorage.setItem("tasks", JSON.stringify(newTask));
   };
   return (
     <TaskContext.Provider
@@ -66,7 +67,7 @@ export const TaskProvider = ({ children }) => {
         setId,
         deleteAll,
         editTask,
-        doneTaskList,
+        doneTaskList
       }}
     >
       {children}
